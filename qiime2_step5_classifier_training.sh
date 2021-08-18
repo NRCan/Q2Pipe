99ba06fc7cf15fb0c1c53c332a8a4d3329736ed6
Patrick Gagné <10926860+Patg13@users.noreply.github.com>
2021-08-18 14:34:58 +0000

    Had to change the classifier training step because it was based on an old method, this one is actual, but much too simple, this step will have to be redone later

--- qiime2_step5_classifier_training.sh
+++ qiime2_step5_classifier_training.sh
@@ -18,25 +18,29 @@ then
 fi
 
 
-fasta_name=$( echo $FASTA_DATABASE_PATH | sed 's/\.[^.]*$//' )
-tax_name=$( echo $TAXO_DATABASE_PATH | sed 's/\.[^.]*$//' )
+#fasta_name=$( echo $FASTA_DATABASE_PATH | sed 's/\.[^.]*$//' )
+#tax_name=$( echo $TAXO_DATABASE_PATH | sed 's/\.[^.]*$//' )
 
-$SINGULARITY_COMMAND qiime tools import \
- --type FeatureData[Sequence] \
- --input-path $FASTA_DATABASE_PATH \
- --output-path $ANALYSIS_NAME.$fasta_name.qza
+#$SINGULARITY_COMMAND qiime tools import \
+# --type FeatureData[Sequence] \
+# --input-path $FASTA_DATABASE_PATH \
+# --output-path $ANALYSIS_NAME.$fasta_name.qza
 
-$SINGULARITY_COMMAND qiime tools import \
- --type FeatureData[Taxonomy] \
- --input-path $TAXO_DATABASE_PATH \
- --output-path $ANALYSIS_NAME.$tax_name.qza \
- --input-format HeaderlessTSVTaxonomyFormat
+#$SINGULARITY_COMMAND qiime tools import \
+# --type FeatureData[Taxonomy] \
+# --input-path $TAXO_DATABASE_PATH \
+# --output-path $ANALYSIS_NAME.$tax_name.qza \
+# --input-format HeaderlessTSVTaxonomyFormat
+
+#$SINGULARITY_COMMAND qiime feature-classifier fit-classifier-naive-bayes \
+# --i-reference-reads $ANALYSIS_NAME.$fasta_name.qza \
+# --i-reference-taxonomy $ANALYSIS_NAME.$tax_name.qza \
+# --o-classifier $CLASSIFIER_OUTPUT_NAME
 
 $SINGULARITY_COMMAND qiime feature-classifier fit-classifier-naive-bayes \
- --i-reference-reads $ANALYSIS_NAME.$fasta_name.qza \
- --i-reference-taxonomy $ANALYSIS_NAME.$tax_name.qza \
+ --i-reference-reads $SEQS_QZA_PATH \
+ --i-reference-taxonomy $TAXO_QZA_PATH \
  --o-classifier $CLASSIFIER_OUTPUT_NAME
-
 
 
 # Manifest file must be done 
