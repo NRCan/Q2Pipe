d87c1f8269d5e0d8f49d4fddaf5e1bae277b704e
Patrick Gagné <10926860+Patg13@users.noreply.github.com>
2021-08-19 19:49:17 +0000

    BUGFIX wrong path for biom input file (biom file is inside the folder created by first export command in script) + optionfile cleanup (clear seperation of step 1.5 and 2) + informations on p_metrics format

--- qiime2_step11_export.sh
+++ qiime2_step11_export.sh
@@ -35,10 +35,10 @@ $SINGULARITY_COMMAND qiime tools export \
 --input-path $ANALYSIS_NAME.rarefied_"$p_sampling_depth"_filtered_table_dn"$p_perc_identity".qza \
 --output-path $ANALYSIS_NAME.rarefied_"$p_sampling_depth"_filtered_table_dn"$p_perc_identity"
 
-biom convert -i $ANALYSIS_NAME.feature-table.biom -o $ANALYSIS_NAME.rarefied_"$p_sampling_depth"_filtered_table_dn"$p_perc_identity".tsv --to-tsv
+biom convert -i $ANALYSIS_NAME.rarefied_"$p_sampling_depth"_filtered_table_dn"$p_perc_identity"/feature-table.biom -o $ANALYSIS_NAME.rarefied_"$p_sampling_depth"_filtered_table_dn"$p_perc_identity".tsv --to-tsv
 
 qiime tools export \
 --input-path $ANALYSIS_NAME.taxo_dn"$p_perc_identity".qza \
---output-path $ANALYSIS_NAME.asv_tax_dirrarefied_"$p_sampling_depth"_dn"$p_perc_identity"
+--output-path $ANALYSIS_NAME.asv_tax_dir_rarefied_"$p_sampling_depth"_dn"$p_perc_identity"
 
 
