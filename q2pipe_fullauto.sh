0aee37570643d63c6ac8c23ca2b04809b96a482a
Patrick Gagné <10926860+Patg13@users.noreply.github.com>
2021-10-25 19:04:04 +0000

    New Feature: Full auto mode; this script can launch the entire pipeline automatically using only a complete option file. it also supports checkpointing and interruption in case of a step error

---
+++ q2pipe_fullauto.sh
@@ -0,0 +1,222 @@
+#!/bin/bash
+
+################################
+#                              #
+#      Qiime 2 Pipeline        #
+#  By: Patrick Gagne (NRCan)   #
+#      "Full Auto" mode        #
+#       October 21, 2021        #
+#                              #
+################################
+
+# This script is designed to launch a full analysis without any user interaction
+
+exit_on_error(){
+   echo "Step error detected"
+   echo "Exiting program"
+   exit 1
+}
+
+
+optionfile=$1
+
+if [ ! $optionfile ]
+then
+    echo "ERROR: you must specify a qiime2 optionfile"
+    exit 1
+fi
+
+source $optionfile
+
+if [ $TEMPORARY_DIRECTORY ]
+then
+    echo "Overriding default temporary directory to $TEMPORARY_DIRECTORY"
+    if [ ! -d $TEMPORARY_DIRECTORY ] || [ ! -w $TEMPORARY_DIRECTORY ]
+    then
+        echo "ERROR: $TEMPORARY_DIRECTORY does not exist or is read only"
+        exit 2
+    fi
+    export TMPDIR=$TEMPORARY_DIRECTORY
+fi
+
+
+if [ ! $Q2P ]
+then
+    echo "WARNING: Q2P environment variable not set"
+    echo "The fullauto mode requires this variable to work"
+    echo "Setting temporary Q2P variable"
+    SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+    export Q2P=$SCRIPT_DIR
+fi
+
+echo "Installation path: $Q2P"
+
+invalid_next=0
+
+if [ ! -e q2pipe_step1.DONE ]
+then
+    echo "Launching Step 1"
+    if [ -e q2pipe_step1.DONE ]
+    then
+        echo "Invalid checkpoint detected. Deleting..."
+        rm q2pipe_step1.DONE
+    fi
+    $Q2P/q2pipe_step1_import.sh $optionfile || exit_on_error
+    touch q2pipe_step1.DONE
+    invalid_next=1
+else
+    echo "Step 1 checkpoint detected...skipping"
+fi
+
+if [ ! -e q2pipe_step2.DONE ] || [ $invalid_next -eq 1 ]
+then
+    echo "Launching Step 2"
+    if [ -e q2pipe_step2.DONE ]
+    then
+        echo "Invalid checkpoint detected. Deleting..."
+        rm q2pipe_step2.DONE
+    fi
+    $Q2P/q2pipe_step2_denoise.sh $optionfile || exit_on_error
+    touch q2pipe_step2.DONE
+    invalid_next=1
+else
+    echo "Step 2 checkpoint detected...skipping"
+fi
+
+if [ ! -e q2pipe_step3.DONE ] || [ $invalid_next -eq 1 ]
+then
+    echo "Launching Step 3"
+    if [ -e q2pipe_step3.DONE ]
+    then
+        echo "Invalid checkpoint detected. Deleting..."
+        rm q2pipe_step3.DONE
+    fi
+    $Q2P/q2pipe_step3_feature_filtering.sh $optionfile || exit_on_error
+    touch q2pipe_step3.DONE
+    invalid_next=1
+else
+    echo "Step 3 checkpoint detected...skipping"
+fi
+
+if [ ! -e q2pipe_step4.DONE ] || [ $invalid_next -eq 1 ]
+then
+    echo "Launching Step 4"
+    if [ -e q2pipe_step4.DONE ]
+    then
+        echo "Invalid checkpoint detected. Deleting..."
+        rm q2pipe_step4.DONE
+    fi
+    $Q2P/q2pipe_step4_denovo_clustering.sh $optionfile || exit_on_error
+    touch q2pipe_step4.DONE
+    invalid_next=1
+else
+    echo "Step 4 checkpoint detected...skipping"
+fi
+
+if [ ! -e q2pipe_step5.DONE ] || [ $invalid_next -eq 1 ]
+then
+    echo "Launching Step 5"
+    if [ -e q2pipe_step5.DONE ]
+    then
+        echo "Invalid checkpoint detected. Deleting..."
+        rm q2pipe_step5.DONE
+    fi
+    $Q2P/q2pipe_step5_classifier_training.sh $optionfile || exit_on_error
+    touch q2pipe_step5.DONE
+    invalid_next=1
+else
+    echo "Step 5 checkpoint detected...skipping"
+fi
+
+if [ ! -e q2pipe_step6.DONE ] || [ $invalid_next -eq 1 ]
+then
+    echo "Launching Step 6"
+    if [ -e q2pipe_step6.DONE ]
+    then
+        echo "Invalid checkpoint detected. Deleting..."
+        rm q2pipe_step6.DONE
+    fi
+    $Q2P/q2pipe_step6_classification.sh $optionfile || exit_on_error
+    touch q2pipe_step6.DONE
+    invalid_next=1
+else
+    echo "Step 6 checkpoint detected...skipping"
+fi
+
+if [ ! -e q2pipe_step7.DONE ] || [ $invalid_next -eq 1 ]
+then
+    echo "Launching Step 7"
+    if [ -e q2pipe_step7.DONE ]
+    then
+        echo "Invalid checkpoint detected. Deleting..."
+        rm q2pipe_step7.DONE
+    fi
+    $Q2P/q2pipe_step7_taxa_filtering.sh $optionfile || exit_on_error
+    touch q2pipe_step7.DONE
+    invalid_next=1
+else
+    echo "Step 7 checkpoint detected...skipping"
+fi
+
+if [ ! -e q2pipe_step8.DONE ] || [ $invalid_next -eq 1 ]
+then
+    echo "Launching Step 8"
+    if [ -e q2pipe_step8.DONE ]
+    then
+        echo "Invalid checkpoint detected. Deleting..."
+        rm q2pipe_step8.DONE
+    fi
+    $Q2P/q2pipe_step8_rarefaction_curve.sh $optionfile || exit_on_error
+    touch q2pipe_step8.DONE
+    invalid_next=1
+else
+    echo "Step 8 checkpoint detected...skipping"
+fi
+
+if [ ! -e q2pipe_step9.DONE ] || [ $invalid_next -eq 1 ]
+then
+    echo "Launching Step 9"
+    if [ -e q2pipe_step9.DONE ]
+    then
+        echo "Invalid checkpoint detected. Deleting..."
+        rm q2pipe_step9.DONE
+    fi
+    $Q2P/q2pipe_step9_rarefy.sh $optionfile || exit_on_error
+    touch q2pipe_step9.DONE
+    invalid_next=1
+else
+    echo "Step 9 checkpoint detected...skipping"
+fi
+
+if [ ! -e q2pipe_step10.DONE ] || [ $invalid_next -eq 1 ]
+then
+    echo "Launching Step 10"
+    if [ -e q2pipe_step10.DONE ]
+    then
+        echo "Invalid checkpoint detected. Deleting..."
+        rm q2pipe_step10.DONE
+    fi
+    $Q2P/q2pipe_step10_metrics.sh $optionfile || exit_on_error
+    touch q2pipe_step10.DONE
+    invalid_next=1
+else
+    echo "Step 10 checkpoint detected...skipping"
+fi
+
+if [ ! -e q2pipe_step11.DONE ] || [ $invalid_next -eq 1 ]
+then
+    echo "Launching Step 11"
+    if [ -e q2pipe_step11.DONE ]
+    then
+        echo "Invalid checkpoint detected. Deleting..."
+        rm q2pipe_step11.DONE
+    fi
+    $Q2P/q2pipe_step11_export.sh $optionfile || exit_on_error
+    touch q2pipe_step11.DONE
+    invalid_next=1
+else
+    echo "Step 11 checkpoint detected...skipping"
+fi
+
+echo "PROGRAM DONE"
