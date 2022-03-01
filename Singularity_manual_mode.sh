9115d22447ce79bc00faca7851aca4d7835db62d
Patrick Gagné <10926860+Patg13@users.noreply.github.com>
2022-03-01 17:13:30 +0000

    Singularity manual mode now support qiime completion (using the scripts from inside the container), the script is using automatic qiime2 version fetching, so no need to upgrade it for each version

--- Singularity_manual_mode.sh
+++ Singularity_manual_mode.sh
@@ -39,8 +39,21 @@ alias qiime="$sing_command qiime"
 alias sing="$sing_command"
 alias singshell="$shell_command"
 
+
+# Activating bash completion 
+if [ ! -d /tmp/qiime2_sing_bashcomp ]
+then
+    mkdir /tmp/qiime2_sing_bashcomp
+fi
+q2_ver=$( $sing_command qiime info | grep "QIIME 2 release" | awk -F: '{ print $2 }' | sed 's/ //g' )
+$sing_command cp /opt/conda/envs/qiime2-$q2_ver/bin/tab-qiime /home/qiime2/q2cli/cache/completion.sh /tmp/qiime2_sing_bashcomp
+sed -i 's/local completion_path=/#local completion_path=/g' /tmp/qiime2_sing_bashcomp/tab-qiime
+line_add=$( grep -n "local completion_path" /tmp/qiime2_sing_bashcomp/tab-qiime | awk -F: '{ print $1 }' )
+sed -i "$line_add i local completion_path=/tmp/qiime2_sing_bashcomp/completion.sh" /tmp/qiime2_sing_bashcomp/tab-qiime
+source /tmp/qiime2_sing_bashcomp/tab-qiime
+
 echo "Singularity aliases ready"
-echo "use 'qiime' to launch qiime commands"
+echo "use 'qiime' to launch qiime commands (you can use [TAB] to complete the commands)"
 echo "use 'sing' to launch other commands within the qiime2 container (ex sing biom)"
 echo "use 'singshell' to directly interact with the singularity container"
 
