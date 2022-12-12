2d6a9adfd1d05f03dd465cc25be70bfb2a068af9
Patrick Gagné <10926860+Patg13@users.noreply.github.com>
2022-12-12 16:09:52 +0000

    New feature: Result extraction based on form file, useful to rapidly isolate needed files for a specific analysis

--- q2pipe_step11_export.sh
+++ q2pipe_step11_export.sh
@@ -153,6 +153,37 @@ then
     fi
 fi
 
+if [ $EXTRACTION_FORM_PATH ]
+then
+    if [ ! -e $EXTRACTION_FORM_PATH ]
+    then
+        echo "ERROR: Extraction form $EXTRACTION_FORM_PATH not found"
+        exit 1
+    fi
+
+    echo "Extracting results files according to $EXTRACTION_FORM_PATH"
+    mkdir "$ANALYSIS_NAME"_form_extraction 2>/dev/null
+    head -n 1 $EXTRACTION_FORM_PATH > "$ANALYSIS_NAME"_form_extraction/extraction_form.tsv
+
+    cat $EXTRACTION_FORM_PATH | sed 's/\t/,/g' | while read line
+    do
+        if [ "$( echo $line | awk -F',' '{ print $2 }' )" != "True" ]
+        then
+            continue
+        fi
+        #echo $line
+        
+        filename=$( echo $line | awk -F',' '{ print $6 }' | sed "s/ANALYSIS_NAME/$ANALYSIS_NAME/g" | sed "s/RARELEVEL/$p_sampling_depth/g" | sed "s/FREQLEVEL/$p_min_frequency/g" | sed "s/SAMPLEVEL/$p_min_samples/g" )
+        echo $filename
+        if [ ! -e $filename ]
+        then
+            echo "WARNING: $filename not found"
+            continue
+        fi
+        cp -R --parent $filename "$ANALYSIS_NAME"_form_extraction/
+        echo $line | sed 's/,/\t/g' >> "$ANALYSIS_NAME"_form_extraction/extraction_form.tsv
+    done
+fi
 
 
 if [ "$GENERATE_ANCOM" == "true" ]
